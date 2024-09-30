7c4509a355e53cfe00fe191d3fb18c9a0abe6716
Aaron Crandall <117679437+AaronCrandall@users.noreply.github.com>
2024-09-30 16:40:01 -0400

    Video completed

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const jwt = require('jsonwebtoken');
+const { expressjwt: exjwt} = require('express-jwt');
+
+const PORT = 3000;
+const secretKey = "My super secret key";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+});
+
+const jwtMW = exjwt({
+    secret: secretKey,
+    algorithms: ['HS256']
+});
+
+let users = [
+    {
+        id: 1,
+        username: 'fabio',
+        password: '123'
+    },
+    {
+        id: 2,
+        username: 'nolasco',
+        password: '456'
+    }
+];
+
+app.post('/api/login', (req, res) => {
+    const {username , password} = req.body;
+
+    for (let user of users){
+        if (username == user.username && password == user.password){
+            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, { expiresIn: '7d'});
+            res.json({
+                success: true,
+                err: null,
+                token
+            })
+            break;
+        }
+        else{
+            res.status(401).json({
+                success: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    }
+});
+
+app.get('/api/dashboard', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Secret content that only logged in people can see.'
+   }) 
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(function (err, req, res, next){
+    if (err.name === 'UnathorizedError'){
+        res.status(401).json({
+            sucess: false,
+            err
+        })
+    }
+    else{
+        next(err);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+})
+
+
